9a38a290f847042fd525eb3e891a04d356647474
Misbah Islam <misbahdevsinn@gmail.com>
2025-08-30 23:38:30 +0500

    updated code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "327915445"
+    serviceWorkerVersion: "4016374644"
   }
 });
